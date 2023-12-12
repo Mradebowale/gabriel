4d7452df0e3801d4d14b8064141fdb197cee8a3b
mradebowale <hamzwalex@gmail.com>
2023-12-12 13:52:20 +0100

    update

--- job/views.py
+++ job/views.py
@@ -8,27 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    if request.method == "POST":
-        first_name = request.POST.get("first_name")
-        middle_name = request.POST.get("middle_name")
-        last_name = request.POST.get("last_name")
-        gender = request.POST.get("Gender")
-        email = request.POST.get("email")
-        home_address = request.POST.get("home_address")
-        phone_number = request.POST.get("phone_number")
-        job_interest = request.POST.get("job_aspired")
-        valid_id = request.POST.get("valid_id")
-        image = request.FILES.get("image")
-        previous_place = request.POST.get("previous_workplace")
-
-        Applicant.objects.create(First_name=first_name, Middle_name=middle_name, 
-                                 Last_name=last_name, Gender=gender, 
-                                 email=email, Home_address=home_address, Phone_number=phone_number,
-                                 Job_aspired=job_interest, Id_document=valid_id, Id_image=image,
-                                 Former_workplace=previous_place)
-        messages.success(request, "Application Info submitted")
-
-    return render(request, "job/index.html")
+   return render(request, "job/index.html")
 
 @login_required
 def jobuploads(request):
@@ -67,16 +47,29 @@ def logins(request):
 # from django.shortcuts import render, redirect
 # from .forms import UserRegistrationForm
 
-# def register_view(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form = UserRegistrationForm()
+def application(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        middle_name = request.POST.get("middle_name")
+        last_name = request.POST.get("last_name")
+        gender = request.POST.get("Gender")
+        email = request.POST.get("email")
+        home_address = request.POST.get("home_address")
+        phone_number = request.POST.get("phone_number")
+        job_interest = request.POST.get("job_aspired")
+        valid_id = request.POST.get("valid_id")
+        image = request.FILES.get("image")
+        previous_place = request.POST.get("previous_workplace")
 
-#     return render(request, 'register_template.html', {'form': form})
+        Applicant.objects.create(First_name=first_name, Middle_name=middle_name, 
+                                 Last_name=last_name, Gender=gender, 
+                                 email=email, Home_address=home_address, Phone_number=phone_number,
+                                 Job_aspired=job_interest, Id_document=valid_id, Id_image=image,
+                                 Former_workplace=previous_place)
+        messages.success(request, "Application Info submitted")
+
+    return render(request, "job/appform.html")
+
 
 
 def applynow(request):
